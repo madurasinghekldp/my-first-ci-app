f52345c7faa1f973c7bb4daef706b56890abb5f5
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-08 10:54:36 +0530

    Add paragraph to App component for clearer instructions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from CI/CD pipeline!</h1>
-
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
